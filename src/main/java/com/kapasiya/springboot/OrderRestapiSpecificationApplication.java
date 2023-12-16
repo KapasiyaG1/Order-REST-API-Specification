c8c2a732fd94ae943c705792cde8f75f770a7333
soyam kapasiya <soyamp232@gmail.com>
2023-12-16 13:58:41 +0530

    basic spring application structure

---
+++ src/main/java/com/kapasiya/springboot/OrderRestapiSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.kapasiya.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRestapiSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRestapiSpecificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kapasiya/springboot/OrderRestapiSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapasiya.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRestapiSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
